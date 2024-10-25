a4f07cf901f6d1a9fba775f8a9287261bffce045
Kruxo <142524330+Kruxo@users.noreply.github.com>
2024-10-25 14:53:49 +0200

    Gissa talet spel

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
 //Test
+
+Random random = new Random();
+int cpuNumber = random.Next(1, 101);
+
+while (true)
+{
+    Console.WriteLine("Gissa ett tal mellan 1 til 100");
+    int userInput = int.Parse(Console.ReadLine()!);
+
+    if (cpuNumber > userInput)
+    {
+        Console.WriteLine("Talet är högre");
+        continue;
+    }
+    else if (cpuNumber < userInput)
+    {
+        Console.WriteLine("Talet är lägre");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Korrekt!");
+        break;
+    }
+}
